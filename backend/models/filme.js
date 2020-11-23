53d9b502b038c30da53e4f9533372ca0b03141e2
Thais Calazans <thaiscalazans02@gmail.com>
2020-11-22 22:39:01 -0300

    Atualizando o backend com alterações feitas entre os dias 21 e 22 de novembro

---
+++ backend/models/filme.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+module.exports = (sequelize, DataTypes) => {
+    let Filme = sequelize.define('Filme',{
+		idFilme: {
+			field: 'idFilme',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+		},		
+		nomeFilme: {
+			field: 'nomeFilme',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		anoFilme: {
+			field: 'anoFilme',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		fkUsuario: {
+			field: 'fkUsuario',
+			allowNull: false,
+			type: DataTypes.INTEGER,
+				references: {
+					model: 'Usuario',
+					key: 'idUsuario'
+				}
+        },
+        sinopseFilme: {
+            field: 'sinopseFilme',
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        imgFilme: {
+            field: 'imgFilme',
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        avaliacaoFilme: {
+            field: 'avaliacaoFilme',
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+	}, 
+	{
+		tableName: 'Filme', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Filme;
+};
---
+++ backend/routes/filme.js
@@ -0,0 +1,49 @@
+var express = require('express');
+const filme = require('../models/filme');
+var router = express.Router();
+var sequelize = require('../models').sequelize;
+var filmes = require("../temp/filme.json") //Lista de filmes do sistema
+var Filme = require('../models').Filme
+
+
+// O Get retorna, ou seja, ele está retornando uma lista de filmes adicionados no sistema de acordo com a var 'filmes'
+router.get('/', function (req,res){
+    Filme.findAndCountAll().then(resultado => {
+        res.json(resultado.rows)
+    }).catch(erro =>{
+        res.status(500).send(erro.message);
+    });
+});
+
+// o Post adiciona, aqui ele vai adicionar um novo filme naquela lista de filmes.
+router.post('/', function (req, res){
+    var idFilme = req.body.idFilme
+    var nomeFilme = req.body.nomeFilme
+    var anoFilme = req.body.anoFilme
+    var fkUsuario = req.body.fkUsuario
+    var sinopseFilme = req.body.sinopseFilme
+    var imgFilme = req.body.imgFilme
+    var avaliacaoFilme = req.body.avaliacaoFilme
+
+    let instrucaoSql = `insert into Filme values (${idFilme},'${nomeFilme}',${anoFilme}, ${fkUsuario}, '${sinopseFilme}', '${imgFilme}', ${avaliacaoFilme}) `;
+	sequelize.query(instrucaoSql, {
+		model: Filme
+	}).then(resultado => {
+        res.send(resultado);
+    }).catch(erro => {
+        res.status(500).send(erro.message);
+    });
+});
+
+// Retorna uma lista de filmes, porém com o filtro de ator, gênero e ano
+router.get('/indicacao', function(req,res){
+    var ator = req.headers.ator
+    var genero = req.headers.genero
+    var ano = req.headers.ano
+
+    
+
+})
+
+
+module.exports = router;
